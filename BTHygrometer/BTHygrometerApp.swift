e2569488e8da37f7f2b2021f17973e4e45737577
rgrachik <112074005+rgrachik@users.noreply.github.com>
2023-04-28 20:10:07 +0300

    Initial Commit

---
+++ BTHygrometer/BTHygrometerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BTHygrometerApp.swift
+//  BTHygrometer
+//
+//  Created by Роман Грачик on 28.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct BTHygrometerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
